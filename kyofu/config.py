18957efa492c6e5c0c7a3e53170ce6c8e54f726b
colajam93 <fs.fake.g1@gmail.com>
2020-02-11 19:13:49 +0900

    Read config from environment variable

--- kyofu/config.py
+++ kyofu/config.py
@@ -1,3 +1,16 @@
+import os
+
+DB_DRIVER = os.getenv('DB_DRIVER', 'mysql+mysqldb')
+DB_USER = os.getenv('DB_USER', 'kyofu')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'kyofu')
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_PORT = os.getenv('DB_PORT', '3306')
+DB_SCHEMA = os.getenv('DB_SCHEMA', 'kyofu')
+DB_OPTION = os.getenv('DB_OPTION', 'charset=utf8mb4')
+
+DB_URL = f'{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_SCHEMA}' + f'?{DB_OPTION}' if DB_OPTION else ''
+SQLALCHEMY_ENGINE_ECHO = os.getenv('SQLALCHEMY_ENGINE_ECHO ', False)
+
 logger_config = {
     'version': 1,
     'formatters': {
--- kyofu/setup.py
+++ kyofu/setup.py
@@ -1,7 +1,8 @@
 def setup_database_connection():
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
+    from kyofu.config import DB_URL, SQLALCHEMY_ENGINE_ECHO
 
-    engine = create_engine('mysql+mysqldb://kyofu:kyofu@172.17.0.1:3306/kyofu?charset=utf8mb4')
+    engine = create_engine(DB_URL, echo=SQLALCHEMY_ENGINE_ECHO)
     session = sessionmaker(bind=engine)()
     return engine, session
